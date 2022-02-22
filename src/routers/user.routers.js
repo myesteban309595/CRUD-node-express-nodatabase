df420a0aa60cda779af8b2db63ee4d0353fe78f0
myesteban309595 <maryoe_95@hotmail.com>
2022-02-21 20:38:19 -0500

    prueba ruta post y get

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -7,7 +7,26 @@ Uroute.get('/', (req,res)=> {
 
     res.json(GetUser())
     console.log(("se han obtenido todos los usuarios ").bgCyan.black)
+    console.log(GetUser());
 })
 
+Uroute.post('/', (req, res) => {
+
+    const {id,name,lastname,phone,email,sex} = req.body;
+
+    const NewUser = {
+        id: id,
+        name:name,
+        lastname: lastname,
+        phone: phone,
+        email:email,
+        sex:sex
+    }
+    AddUser(NewUser);
+
+    res.json("se ha actualizado el usuario")
+    console.log(("se ha creado un nuevo usuario ").bgGreen.black);
+    console.log(NewUser);
+});
 module.exports = Uroute;
 //validacion ? res.json("se ha actualizado exitosamente el usuario") : res.status(400).json("el usuario o el id no existen");
